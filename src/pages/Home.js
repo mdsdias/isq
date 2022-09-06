e9b1dcb62f3c30d7a7dba66033e2766373b07e33
legumii <isqne@pm.me>
2022-09-05 22:36:43 -0300

    resposive?

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,10 @@ import { Row, Column, Img, Text, List } from "components";
 const Home = () => {
   return (
     <>
-      <Row className="bg-white_A700 font-roboto items-center h-[100%]">
-        <aside className="items-center w-[30%] h-[100%] md:w-full">
-          <Column className="bg-gradient">
-            <Column className="items-center justify-center h-screen">
+      <Row className="bg-white_A700 font-roboto">
+        <aside className="mx-auto w-full h-full lg:w-[30%] lg:h-screen">
+          <Column className="justify-center bg-gradient overflow-hidden h-screen">
+            <Column className="flex items-center scale-[2.1] overflow-clip lg:scale-[0.]">
               <Img
                 src="https://avatars.githubusercontent.com/u/79223936?v=4"
                 className="rounded-radius49 w-[300px]"
@@ -31,60 +31,68 @@ const Home = () => {
             </Column>
           </Column>
         </aside>
-        <Column className="bg-white_A700 ml-[15px] w-[70%] h-screen flex md:hidden">
+        <Column className="bg-white_A700 ml-0 w-0 h-0 hidden lg:ml-[15px] lg:h-full lg:flex lg:w-[70%]">
           <Text className="font-hairline text-[53px] text-black_900 w-[auto]">
             Projetos
           </Text>
-          <Column className=" mt-[15px] w-full">
+          <Column className="mt-[15px] w-full">
             <List
-              className=" grid grid-cols-3 gap-[10px] min-h-[auto] max-w-[auto]"
+              className=" grid grid-cols-3 gap-[15px]"
               orientation="horizontal"
             >
               <Column
-                className="bg-cover bg-no-repeat bg-center bg-white_A700 hover:cursor-pointer items-center justify-center p-[21px] rounded-radius25 shadow-bs hover:shadow-bs1 w-[100%]"
+                className="bg-cover bg-repeat bg-center bg-white_A700 hover:cursor-pointer items-center justify-center p-[21px] rounded-radius25 shadow-bs hover:shadow-bs1"
                 style={{
                   backgroundImage: "url('images/img_card.png')",
                 }}
               >
-                <Text className="font-hairline xl:mb-[3px] lg:mb-[3px] 2xl:mb-[4px] 3xl:mb-[5px] xl:mt-[11px] 2xl:mt-[12px] 3xl:mt-[14px] lg:mt-[9px] lg:text-[34px] xl:text-[39px] 2xl:text-[44px] 3xl:text-[53px] text-white_A700 w-[auto]">
+                <Text className="font-hairline mb-[37px] mt-[35px] text-[34px] text-white_A700 w-[auto]">
                   westing
                 </Text>
               </Column>
               <Column
-                className="bg-cover bg-no-repeat bg-center bg-white_A700 hover:cursor-pointer items-center justify-center p-[21px] rounded-radius25 shadow-bs hover:shadow-bs1 w-[100%]"
+                className="bg-cover bg-repeat bg-center bg-white_A700 hover:cursor-pointer items-center justify-center p-[21px] rounded-radius25 shadow-bs hover:shadow-bs1"
                 style={{
                   backgroundImage: "url('images/img_card_200X300.png')",
                 }}
               >
-                <Text className="font-hairline mb-[3px] lg:text-[34px] xl:text-[39px] 2xl:text-[44px] 3xl:text-[53px] text-white_A700 w-[auto]">
+                <Text className="font-hairline mb-[37px] mt-[35px] text-[34px] text-white_A700 w-[auto]">
                   tob
                 </Text>
               </Column>
               <Column
-                className="bg-cover bg-no-repeat bg-center bg-white_A700 hover:cursor-pointer items-center justify-center p-[21px] rounded-radius25 shadow-bs hover:shadow-bs1 w-[100%]"
+                className="bg-cover bg-repeat bg-center bg-white_A700 hover:cursor-pointer items-center justify-center p-[21px] rounded-radius25 shadow-bs hover:shadow-bs1"
                 style={{
                   backgroundImage: "url('images/img_card_1.png')",
                 }}
-              >
-                <Text className="font-hairline lg:mb-[37px] xl:mb-[43px] 2xl:mb-[48px] 3xl:mb-[58px] lg:mt-[35px] xl:mt-[40px] 2xl:mt-[46px] 3xl:mt-[55px] lg:text-[34px] xl:text-[39px] 2xl:text-[44px] 3xl:text-[53px] text-white_A700 w-[auto]">
+              >  
+              {/* font-hairline mb-[37px] mt-[35px] text-[34px] text-white_A700 w-[auto] */}
+                <Text className="font-hairline mb-[37px] mt-[35px] text-[34px] text-white_A700 w-[auto]">
                   GrabberTool
                 </Text>
               </Column>
             </List>
-            <Text className="font-hairline lg:mt-[13px] xl:mt-[14px] 2xl:mt-[16px] 3xl:mt-[20px] lg:text-[34px] xl:text-[39px] 2xl:text-[44px] 3xl:text-[53px] text-black_900 w-[auto]">
+            <Text className="font-hairline mt-[11px] text-[44px] text-black_900">
               Sobre mim
             </Text>
-            <Text className="font-hairline lg:mt-[13px] xl:mt-[14px] 2xl:mt-[16px] 3xl:mt-[20px] lg:text-[17px] xl:text-[19px] 2xl:text-[22px] 3xl:text-[26px] text-black_900 w-[auto]">
+            <Text className="font-hairline mt-[11px] text-[22px] text-black_900">
               Um garoto querendo aprender sobre um pouco de tudo
             </Text>
-            <Text className="font-hairline xl:mt-[10px] 2xl:mt-[11px] 3xl:mt-[13px] lg:mt-[8px] lg:text-[34px] xl:text-[39px] 2xl:text-[44px] 3xl:text-[53px] text-black_900 w-[auto]">
+            <Text className="font-hairline mt-[11px] text-[44px] text-black_900">
               Skills
             </Text>
+            <List className="grid grid-flow-col auto-cols-max gap-2">
             <Img
               src="images/img_rectangle4.png"
-              className="mt-[9px] rounded-radius25 w-[96px]"
+              className="rounded-radius25 w-[62px]"
               alt="RectangleFour"
             />
+             <Img
+              src="images/img_rectangle4.png"
+              className="rounded-radius25 w-[62px]"
+              alt="RectangleFour"
+            />
+            </List>
           </Column>
         </Column>
       </Row>
